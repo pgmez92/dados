93a2dda6125bc9350c16b72e0de8abb0806ba909
Pablo GómGar <pgmez92@gmail.com>
2022-06-23 22:10:45 +0200

    ánimo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function satan() {
   let num_dados, num_caras;
   let arr_tirada = [];
-  let num_repes = {};
 
   let tiradas = document.querySelector('[id="tiradas"]');
   let repes = document.querySelector('[id="repes"]');
@@ -13,10 +12,37 @@ function satan() {
     arr_tirada.push(Math.ceil(Math.random() * num_caras));
   }
 
-  arr_tirada.forEach(function (x) {
-    num_repes[x] = (num_repes[x] || 0) + 1;
-  });
-
   tiradas.textContent = arr_tirada;
-  repes.textContent = JSON.stringify(num_repes);
+  arr_tirada.sort();
+
+  /* subdividir el arr_tirada en arrays parejos y  contar su length */
+  /* for (let index = 0; index < arr_tirada.length; ++index) {
+    let repatencia = arr_tirada.filter((valor) => valor == arr_tirada[index]);
+    repes.textContent += repatencia;
+  } */
+
+  /* TUSMUERTOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS */
+  /*  let arr_repes = []; 
+    for (let index = 0; index <= arr_tirada.length; ++index) {
+    let count = 1;
+    let num_repe = { valor: arr_tirada[index], veces: 0 };
+
+    for (let i = index + 1; i <= arr_tirada.length; ++i) {
+      if (arr_tirada[index] === arr_tirada[i]) {
+        ++count;
+      }
+    }
+
+    if (count > 1) {
+      num_repe.veces = count;
+      arr_repes.push(num_repe);
+    }
+
+    repes.textContent = JSON.stringify(arr_repes); */
+
+  let meagotas = {};
+  arr_tirada.forEach(function (x) {
+    meagotas[x] = (meagotas[x] || 0) + 1;
+  });
+  repes.textContent = JSON.stringify(meagotas);
 }
